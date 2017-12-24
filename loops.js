843d4c8f5a29c6b52bb9fc526d9284f8052c2ae2
winter mutt <sejongsan@hotmail.com>
2017-12-24 22:15:58 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,14 +1,13 @@
-var array = [];
 function forLoop(array){
-    for (let i = 0; i < 25; i++){
-        if(i == 1){
-            array.push("I am 1 strange loop.");
-        }
-    else{
-		    array.push(`I am ${i} strange loops.`);
-        }
+  for(var i = 0; i < 25; i++){
+    if(i === 1){
+      array.push("I am a strange loop.");
     }
-    return array;
+    else {
+      array.push("I am ${i} strange loops.");
+    }
+  }
+  return array;
 }
 
 function whileLoop(number){
@@ -22,8 +21,8 @@ function doWhileLoop(array){
   function maybeTrue(){
     return Math.random() >= 0.5;
   }
-  do{
-    array.shift();
-  }while(maybeTrue() && array.length > 0);
+  while (array.length > 0 && maybeTrue()) {
+    array.pop();
+  }
   return array;
 }
